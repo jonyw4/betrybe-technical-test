6086d50cd204f7d6d07429e8ca149b469bf42db0
Jonathan Célio <jonathan.clio@hotmail.com>
2020-12-09 17:50:09 +0000

    fix: update location of the currency.json file

--- src/api/btc/currencies.service.ts
+++ src/api/btc/currencies.service.ts
@@ -2,18 +2,16 @@ import { Currencies, CurrenciesExchanges } from './types';
 import fs from 'fs';
 import path from 'path';
 
-const FILE_PATH = 'currencies.json';
+const FILE_NAME = 'currencies.json';
 
 export class CurrenciesService {
+  constructor(private currencyFilePath = path.resolve('./public', FILE_NAME)) {}
   public async getCurrencies(): Promise<Currencies[]> {
     const currenciesExchanges = await this.getCurrenciesExchanges();
     return Object.keys(currenciesExchanges).map((c) => c as Currencies);
   }
   public async getCurrenciesExchanges(): Promise<CurrenciesExchanges> {
-    const data = await fs.promises.readFile(
-      path.resolve(__dirname, FILE_PATH),
-      'utf8'
-    );
+    const data = await fs.promises.readFile(this.currencyFilePath, 'utf8');
     return JSON.parse(data.toString());
   }
   public async updateCurrencyExchange(
@@ -26,10 +24,7 @@ export class CurrenciesService {
       [currency]: Number(value).toFixed(3)
     };
     try {
-      await fs.promises.writeFile(
-        path.resolve(__dirname, FILE_PATH),
-        JSON.stringify(data)
-      );
+      await fs.promises.writeFile(this.currencyFilePath, JSON.stringify(data));
       return true;
     } catch {
       return false;

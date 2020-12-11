6cd610a9c2db355b60ac3a550d6d16d379761400
Jonathan Célio <jonathan.clio@hotmail.com>
2020-12-11 13:24:15 +0000

    refactor: transform btc rate to input

---
+++ src/components/BtcRateInput/BtcRateInput.component.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+export function BtcRateInput({
+  rate,
+  title,
+  onChange = undefined,
+  disabled = false
+}: BtcRateInputProps) {
+  return (
+    <div className="btc-rate">
+      <div className="btc-rate__title">{title}</div>
+      <input
+        className="btc-rate__rate"
+        type="number"
+        name={title}
+        value={rate}
+        onChange={onChange}
+        disabled={disabled}
+        step={0.1}
+      />
+    </div>
+  );
+}
+
+interface BtcRateInputProps {
+  rate: number;
+  title: string;
+  disabled?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
---
+++ src/components/BtcRateInput/BtcRateInput.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { mount } from 'cypress-react-unit-test';
+import { BtcRateInput } from './BtcRateInput.component';
+
+describe('components > BtcRateInput', () => {
+  it('should render a default BtcRateInput correctly', () => {
+    mount(<BtcRateInput rate={10} title="Titulo" />);
+    cy.get('.btc-rate').should('be.visible');
+    cy.get('.btc-rate__title').contains('Titulo');
+    cy.get('.btc-rate__rate').should('have.value', '10');
+  });
+  it('should render a disabled BtcRateInput correctly', () => {
+    mount(<BtcRateInput rate={10} title="Titulo" disabled />);
+    cy.get('.btc-rate').should('be.visible');
+    cy.get('.btc-rate__title').contains('Titulo');
+    cy.get('.btc-rate__rate').should('have.a.property', 'disabled');
+  });
+  it('should render a BtcRateInput and try exec onClick function correctly', () => {
+    const onClick = () => null;
+    cy.spy(onClick);
+    mount(<BtcRateInput rate={10} title="Titulo" onChange={onClick} />);
+    cy.get('.btc-rate__rate')
+      .type('5')
+      .should(() => expect(onClick).to.be.calledOnce);
+  });
+});

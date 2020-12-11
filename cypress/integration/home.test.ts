e02aff2598d01a5dc74c76a47a4d96008078ae43
Jonathan Célio <jonathan.clio@hotmail.com>
2020-12-11 13:24:54 +0000

    test: add e2e test to home

--- cypress/integration/home.test.ts
+++ cypress/integration/home.test.ts
@@ -1,5 +1,20 @@
 describe('Home Page', () => {
-  it('Visit the home page', () => {
+  it('Visit the home page and try to update the value', () => {
     cy.visit('/');
+    let brlInputValue: number;
+
+    // Get the original BRL value
+    cy.get('input[name="BRL"]').then((elem) => {
+      brlInputValue = Cypress.$(elem).val() as number;
+    });
+
+    // Update BTC value
+    cy.get('input[name="BTC"]').type('10');
+
+    // Get the new value and check if is correct
+    cy.get('input[name="BRL"]').should((elem) => {
+      const inputValue = Cypress.$(elem).val();
+      return brlInputValue * 10 === inputValue;
+    });
   });
 });
